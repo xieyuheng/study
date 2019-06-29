cda47346832584a97dec9701ba1098df0458829c
Xie Yuheng <xyheme@gmail.com>
2019-06-29 21:09:00 +0800

    placeholder test

---
+++ src/test/scala/xieyuheng/cicadascript/CicadaScriptSpec.scala
@@ -0,0 +1,12 @@
+import org.scalatest._
+import xieyuheng.cicadascript._
+
+class CicadaScriptSpec extends FlatSpec with Matchers {
+  "placeholder" should "pass" in {
+    assert(true)
+  }
+
+  it should "pass again" in {
+    assert(true)
+  }
+}

b7fb54f8ff75e892404e8a1c83c001f8787aff50
Xie Yuheng <xyheme@gmail.com>
2019-07-13 18:57:54 +0800

    - Monoid as Category

--- examples/abstract-algebra/Monoid.cs
+++ examples/abstract-algebra/Monoid.cs
@@ -1,7 +1,25 @@
 module abstract-algebra
 
+import category-theory._
+
 class Monoid <: Semigroup {
   id: E
   left_id(x: E): id * x == x
   right_id(x: E): x * id == x
+}
+
+extend Monoid {
+  asCategory = Category {
+    Object = Unit
+    Morphism(_: Unit, _: Unit) = E
+
+    id(_: Unit): E = this.id
+
+    compose = mul
+
+    left_id = this.left_id
+    right_id = this.left_id
+
+    associative = this.associative
+  }
 }

84bdab8b1af865ba769eed3accced66e9dae69ae
Xie Yuheng <xyheme@gmail.com>
2019-07-20 22:47:33 +0800

    up

--- examples/order_theory/PartialOrder.cs
+++ examples/order_theory/PartialOrder.cs
@@ -2,6 +2,9 @@ module order_theory
 
 class PartialOrder <: PreOrder {
   antisymmetric(a <= b, b <= a): a == b
+
+  Cover[-<](a: E, b: E): Type =
+    (a < b, (x: E, a <= x < b) => x == a)
 }
 
 // no cycle
--- examples/order_theory/PreOrder.cs
+++ examples/order_theory/PreOrder.cs
@@ -5,6 +5,9 @@ class PreOrder {
   Pre[<=](E, E): Type
   reflexive(a: E): a <= a
   transitive(a <= b, b <= c): a <= c
+
+  StrictPre[<](a: E, b: E): Type =
+    (a <= b, a != b)
 }
 
 // thin category

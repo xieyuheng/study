21226e388023107a32b266ff27cff05742a6c549
Xie Yuheng <xyheme@gmail.com>
2019-08-19 18:21:09 +0800

    remove old ts code

--- old/ts/prelude.ts
+++
@@ -1,4 +0,0 @@
-export { bool } from "./std/bool"
-export { nat } from "./std/nat"
-export { list } from "./std/list"
-export { vect } from "./std/vect"
--- old/ts/this.ts
+++
@@ -1,9 +0,0 @@
-import assert from "assert"
-import * as ut from "../util"
-import * as gs from "./game-semantics"
-import { module_t } from "./core"
-
-// export
-// class this_t extends gs.game_t {
-
-// }

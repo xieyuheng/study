26cc8c86fc84bfe22bc088c993a7e3452a6b2ab5
Xie Yuheng <xyheme@gmail.com>
2019-08-23 01:08:42 +0800

    - pre

--- src/main/scala/cicada/pre.scala
+++ src/main/scala/cicada/pre.scala
@@ -2,6 +2,12 @@ package cicada
 
 import cicada.pretty._
 
+// subtype relation, and not just subtype relation,
+//   because a value pre its type.
+
 object pre {
-  // TODO
+  def apply(x: Value, y: Value, bind: Bind, env: Env): Boolean = {
+    val (z, newBind) = join.yieldBind(x, y, bind, env)
+    z == walk.deep(y, newBind)
+  }
 }

2d07f8dd6b9bce535a85f0ebcb67f73ef680c645
Xie Yuheng <xyheme@gmail.com>
2019-09-03 22:29:39 +0800

    up

--- src/main/scala/xieyuheng/partech/ParserTest.scala
+++ src/main/scala/xieyuheng/partech/ParserTest.scala
@@ -11,48 +11,50 @@ object ParserTest extends App {
     }
   }
 
-//   Seq(
-//     "(true false)",
-//     "(true false true)",
-//     "(true ((((false)))))",
-//   ).foreach { show(bool_sexp.bool_sexp, _) }
+  Seq(
+    "(true false)",
+    "(true false true)",
+    "(true ((((false)))))",
+  ).foreach { show(bool_sexp.bool_sexp, _) }
 
-//   Seq(
-//     "tom, dick and harry",
-//   ).foreach { show(tom_dick_and_harry.tom_dick_and_harry, _) }
+  Seq(
+    "tom, dick and harry",
+  ).foreach { show(tom_dick_and_harry.tom_dick_and_harry, _) }
 
-//   Seq(
-//     "t,d&h",
-//   ).foreach { show(tdh.tdh, _) }
+  Seq(
+    "t,d&h",
+  ).foreach { show(tdh.tdh, _) }
 
-//   Seq(
-//     "t,d&h",
-//   ).foreach { show(tdh_left.tdh_left, _) }
-
-//   Seq(
-//     "1 + 0",
-//     "1 + 1 + 1 + 0",
-//     "0 + 0",
-//     "1 + 2",
-//   ).foreach { show(bin_sum.bin_sum, _) }
+  Seq(
+    "t,d&h",
+  ).foreach { show(tdh_left.tdh_left, _) }
 
   Seq(
     "1 + 0",
     "1 + 1 + 1 + 0",
     "0 + 0",
     "1 + 2",
+  ).foreach { show(bin_sum.bin_sum, _) }
+
+  Seq(
+    "1 + 0",
+    "1 + 1 + 1 + 0",
+    "0 + 0",
+    "1 + 2",
+    "1 + 9",
+    "1 + 9 + 5",
   ).foreach { show(dec_sum.dec_sum, _) }
 
-//   Seq(
-//     "ab",
-//     "abab",
-//     "aabb",
-//     "aab",
-//   ).foreach { show(ab.ab, _) }
+  Seq(
+    "ab",
+    "abab",
+    "aabb",
+    "aab",
+  ).foreach { show(ab.ab, _) }
 
-//   Seq(
-//     "abc",
-//     "aabbcc",
-//   ).foreach { show(abc.abc, _) }
+  Seq(
+    "abc",
+    "aabbcc",
+  ).foreach { show(abc.abc, _) }
 
 }

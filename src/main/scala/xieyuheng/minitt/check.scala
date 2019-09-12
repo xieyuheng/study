3bf513502bb7ecf0a2207aa8cc0123d8cfbbf9be
Xie Yuheng <xyheme@gmail.com>
2019-09-13 03:49:43 +0800

    check_decl

--- src/main/scala/xieyuheng/minitt/check.scala
+++ src/main/scala/xieyuheng/minitt/check.scala
@@ -3,14 +3,31 @@ package xieyuheng.minitt
 object check {
 
   def check_decl(i: Int, env: Env, ctx: Ctx, decl: Decl): Either[Err, Ctx] = {
+    decl match {
+      case Let(pat, t_exp, e) =>
+        for {
+          _ <- check_type(i, env, ctx, t_exp)
+          t = eval(t_exp, env)
+          ctx1 <- ctx.ext(pat, t, eval(e, env))
+        } yield ctx1
+      case decl @ Letrec(pat, t_exp, e) =>
+        for {
+          _ <- check_type(i, env, ctx, t_exp)
+          t = eval(t_exp, env)
+          fresh = ValNeu(NeuVar(readback.fresh_name(i)))
+          ctx1 <- ctx.ext(pat, t, fresh)
+          _ <- check(i + 1, EnvPat(pat, fresh, env), ctx1, e, t)
+          v = eval(e, EnvDecl(decl, env))
+          ctx2 <- ctx.ext(pat, t, v)
+        } yield ctx2
+    }
+  }
+
+  def check_type(i: Int, env: Env, ctx: Ctx, t: Exp): Either[Err, Unit] = {
     ???
   }
 
-  def check_univ(i: Int, env: Env, ctx: Ctx, t: Exp): Either[Err, Unit] = {
-    ???
-  }
-
-  def check_type(i: Int, env: Env, ctx: Ctx, e: Exp, t: Val): Either[Err, Unit] = {
+  def check(i: Int, env: Env, ctx: Ctx, e: Exp, t: Val): Either[Err, Unit] = {
     ???
   }
 

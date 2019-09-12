6904f3abbcb87622f92615c7428ad69554a4ae8e
Xie Yuheng <xyheme@gmail.com>
2019-09-12 18:45:06 +0800

    up

--- src/main/scala/xieyuheng/minitt/minitt.scala
+++ src/main/scala/xieyuheng/minitt/minitt.scala
@@ -6,7 +6,7 @@ object minitt {
 
   def version: String = "0.0.1"
 
-  def get_args(args: Array[String], name: String, arity: Int): Option[Array[String]] = {
+  def opt(args: Array[String], name: String, arity: Int): Option[Array[String]] = {
     val i = args.indexOf(name)
     if (i == -1) {
       None
@@ -29,19 +29,19 @@ object minitt {
 
   def main(args: Array[String]): Unit = {
 
-    get_args(args, "--help", 0).foreach {
+    opt(args, "--help", 0).foreach {
       case _ =>
         print_help()
         System.exit(0)
     }
 
-    get_args(args, "--version", 0).foreach {
+    opt(args, "--version", 0).foreach {
       case _ =>
         println(version)
         System.exit(0)
     }
 
-    get_args(args, "--eval", 1).foreach {
+    opt(args, "--eval", 1).foreach {
       case Array(file_name) =>
         run_file(file_name)
         System.exit(0)

02a6c94eab3bb9e7dc1852aae8ae9a47a68b1639
Xie Yuheng <xyheme@gmail.com>
2019-09-19 08:31:44 +0800

    up

--- src/main/scala/xieyuheng/cicada/Exp.scala
+++ src/main/scala/xieyuheng/cicada/Exp.scala
@@ -1,7 +1,5 @@
 package xieyuheng.cicada
 
-import scala.collection.immutable.ListMap
-
 sealed trait Exp
 
 final case class Var(name: String) extends Exp
@@ -14,20 +12,20 @@ final case class Ap(target: Exp, arg: Exp) extends Exp
 
 final case class Club(
   name: String,
-  fileds: ListMap[String, (Exp, Option[Exp])],
+  fileds: List[(String, Exp, Option[Exp])],
   members: List[Member],
 ) extends Exp
 final case class Choice(target: Exp, map: Map[String, Exp]) extends Exp
 
 final case class Member(
   name: String,
-  fileds: ListMap[String, (Exp, Option[Exp])],
+  fileds: List[(String, Exp, Option[Exp])],
   club_name: String,
 ) extends Exp
 
 final case class Record(
   name: String,
-  fileds: ListMap[String, (Exp, Option[Exp])],
+  fileds: List[(String, Exp, Option[Exp])],
   super_names: List[String],
 ) extends Exp
 

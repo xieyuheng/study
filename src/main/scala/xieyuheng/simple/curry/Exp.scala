ad78edbc96b3444b32e6aeafa2f1a416cd23fcf8
Xie Yuheng <xyheme@gmail.com>
2019-10-02 13:46:11 +0800

    up

--- src/main/scala/xieyuheng/simple/curry/Exp.scala
+++ src/main/scala/xieyuheng/simple/curry/Exp.scala
@@ -5,6 +5,19 @@ final case class Var(name: String) extends Exp
 final case class Ap(target: Exp, arg: Exp) extends Exp
 final case class Fn(arg_name: String, body: Exp) extends Exp
 
+object Exp {
+  def free_variables(exp: Exp): Set[String] = {
+    exp match {
+      case Var(name: String) =>
+        Set(name)
+      case Ap(target: Exp, arg: Exp) =>
+        free_variables(target) ++ free_variables(arg)
+      case Fn(arg_name: String, body: Exp) =>
+        free_variables(body) - arg_name
+    }
+  }
+}
+
 sealed trait Type
 final case class Atom(name: String) extends Type
 final case class Arrow(ante: Type, succ: Type) extends Type

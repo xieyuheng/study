98de61dc4b078ccbe01b897157eb29d91feb140f
Xie Yuheng <xyheme@gmail.com>
2019-10-07 18:57:30 +0800

    - make a lang framework for eopl

--- src/main/scala/xieyuheng/eopl/lang_let/lang_let.scala
+++ src/main/scala/xieyuheng/eopl/lang_let/lang_let.scala
@@ -1,96 +1,18 @@
 package xieyuheng.eopl.lang_let
 
+import xieyuheng.eopl.Lang
+
 import xieyuheng.partech.Parser
 
-object lang_let {
-
-  def version: String = "0.0.1"
-
-  def opt(args: Array[String], name: String, arity: Int): Option[Array[String]] = {
-    val i = args.indexOf(name)
-    if (i == -1) {
-      None
-    } else {
-      Some(args.slice(i + 1, i + 1 + arity))
-    }
-  }
-
-  def print_help(): Unit = {
-    val usage = s"""
-        |lang_let ${version}
-        |
-        |usage:
-        |  -e, --eval <file_path> [default]
-        |  -v, --version
-        |  -h, --help
-        """.stripMargin
-    println(usage)
-  }
-
-  def main(args: Array[String]): Unit = {
-
-    opt(args, "-h", 0).foreach {
-      case _ =>
-        print_help()
-        System.exit(0)
-    }
-
-    opt(args, "--help", 0).foreach {
-      case _ =>
-        print_help()
-        System.exit(0)
-    }
-
-    opt(args, "-v", 0).foreach {
-      case _ =>
-        println(version)
-        System.exit(0)
-    }
-
-    opt(args, "--version", 0).foreach {
-      case _ =>
-        println(version)
-        System.exit(0)
-    }
-
-    opt(args, "--eval", 1).foreach {
-      case Array(file_path) =>
-        run_file(file_path)
-        System.exit(0)
-    }
-
-    if (args.length == 1) {
-      val file_path = args(0)
-      run_file(file_path)
-      System.exit(0)
-    }
-
-    print_help()
-
-    System.exit(0)
-  }
-
-  def run_file(file_path: String): Unit = {
-    val path = os.Path(file_path, base = os.pwd)
-
-    if (!os.isFile(path)) {
-      println(s"not a file: ${path}")
-      System.exit(1)
-    }
-
-    val code = os.read(path)
-
-    Parser(grammar.lexer, grammar.exp).parse(code) match {
+object lang_let extends Lang(
+  "lang_let", "0.0.1", {
+    case code => Parser(grammar.lexer, grammar.exp).parse(code) match {
       case Right(tree) =>
-        // val module = grammar.module_matcher(tree)
-        // module.run()
         val exp = grammar.exp_matcher(tree)
         println(exp)
       case Left(error) =>
         println(s"[parse_error] ${error.msg}")
-        println(s"- file: ${file_path}")
         System.exit(1)
     }
   }
-
-}
+)

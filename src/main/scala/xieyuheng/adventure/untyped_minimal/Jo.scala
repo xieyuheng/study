5e1d11bd28ab9efdb8d94b7436da45b11c8dee0e
Xie Yuheng <xyheme@gmail.com>
2019-10-11 06:44:53 +0800

    up

--- src/main/scala/xieyuheng/adventure/untyped_minimal/Jo.scala
+++
@@ -1,7 +0,0 @@
-package xieyuheng.adventure.untyped_minimal
-
-sealed trait Jo
-final case class Var(name: String) extends Jo
-final case class Let(name: String) extends Jo
-final case class JoJo(list: List[Jo]) extends Jo
-final case class Define(name: String, jojo: JoJo) extends Jo
--- src/main/scala/xieyuheng/adventure/untyped_minimal/Val.scala
+++
@@ -1,4 +0,0 @@
-package xieyuheng.adventure.untyped_minimal
-
-sealed trait Val
-final case class ValJoJo(list: List[Jo], env: Env) extends Val

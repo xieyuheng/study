460f89305762d7fc6b2ae89fbf09fb0c5df1453c
Xie Yuheng <xyheme@gmail.com>
2019-10-13 05:19:53 +0800

    up

---
+++ src/main/scala/xieyuheng/eopl/lang_mutable_pairs/Val.scala
@@ -0,0 +1,8 @@
+package xieyuheng.eopl.lang_mutable_pairs
+
+sealed trait Val
+final case class ValNum(num: Int) extends Val
+final case class ValBool(bool: Boolean) extends Val
+final case class ValFn(name: String, body: Exp, env: Env) extends Val
+final case class ValSole() extends Val
+final case class ValRef(address: Int) extends Val

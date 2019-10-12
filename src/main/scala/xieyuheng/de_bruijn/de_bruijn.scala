558965a9a05e7aa5ad781ff44b56ac124a24aab9
Xie Yuheng <xyheme@gmail.com>
2019-10-12 19:48:10 +0800

    - use mini_interpreter.scala as much as possible

--- src/main/scala/xieyuheng/de_bruijn/de_bruijn.scala
+++ src/main/scala/xieyuheng/de_bruijn/de_bruijn.scala
@@ -1,94 +1,18 @@
 package xieyuheng.de_bruijn
 
+import xieyuheng.util.mini_interpreter
+
 import xieyuheng.partech.Parser
 
-object de_bruijn {
-
-  def version: String = "0.0.1"
-
-  def opt(args: Array[String], name: String, arity: Int): Option[Array[String]] = {
-    val i = args.indexOf(name)
-    if (i == -1) {
-      None
-    } else {
-      Some(args.slice(i + 1, i + 1 + arity))
-    }
-  }
-
-  def print_help(): Unit = {
-    val usage = s"""
-        |de_bruijn ${version}
-        |
-        |usage:
-        |  -e, --eval <file_path> [default]
-        |  -v, --version
-        |  -h, --help
-        """.stripMargin
-    println(usage)
-  }
-
-  def main(args: Array[String]): Unit = {
-
-    opt(args, "-h", 0).foreach {
-      case _ =>
-        print_help()
-        System.exit(0)
-    }
-
-    opt(args, "--help", 0).foreach {
-      case _ =>
-        print_help()
-        System.exit(0)
-    }
-
-    opt(args, "-v", 0).foreach {
-      case _ =>
-        println(version)
-        System.exit(0)
-    }
-
-    opt(args, "--version", 0).foreach {
-      case _ =>
-        println(version)
-        System.exit(0)
-    }
-
-    opt(args, "--eval", 1).foreach {
-      case Array(file_path) =>
-        run_file(file_path)
-        System.exit(0)
-    }
-
-    if (args.length == 1) {
-      val file_path = args(0)
-      run_file(file_path)
-      System.exit(0)
-    }
-
-    print_help()
-
-    System.exit(0)
-  }
-
-  def run_file(file_path: String): Unit = {
-    val path = os.Path(file_path, base = os.pwd)
-
-    if (!os.isFile(path)) {
-      println(s"not a file: ${path}")
-      System.exit(1)
-    }
-
-    val code = os.read(path)
-
+object de_bruijn extends mini_interpreter (
+  "de_bruijn", "0.0.1", { case code =>
     Parser(grammar.lexer, grammar.module).parse(code) match {
       case Right(tree) =>
         val module = grammar.module_matcher(tree)
         module.run()
       case Left(error) =>
         println(s"[parse_error] ${error.msg}")
-        println(s"- file: ${file_path}")
         System.exit(1)
     }
   }
-
-}
+)

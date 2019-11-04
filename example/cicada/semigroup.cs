5531be045f44662e2862230fc47ff7ee9a94b48c
Xie Yuheng <xyheme@gmail.com>
2019-11-04 13:11:56 +0800

    up

--- example/cicada/semigroup.cs
+++
@@ -1,9 +0,0 @@
-class semigroup_t {
-  E : type
-  mul : (x: E, y: E) -> E
-  associative : (x: E, y: E, z: E) ->
-    eqv_t(E, mul(x, mul(y, z)), mul(mul(x, y), z))
-}
-
-semigroup : semigroup_t
-semigroup = semigroup_t(E, mul, associative)

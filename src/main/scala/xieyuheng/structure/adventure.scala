ddf55a35500687c684258f7939ea008f630448ff
Xie Yuheng <xyheme@gmail.com>
2019-11-27 16:48:26 +0800

    up

--- src/main/scala/xieyuheng/structure/adventure.scala
+++ src/main/scala/xieyuheng/structure/adventure.scala
@@ -1,27 +1,27 @@
 package xieyuheng.structure
 
 abstract class adventure_t {
-  type jojo_t
+  type elem_t
 
-  def cut(x: jojo_t): jojo_t
+  def cut(x: elem_t): elem_t
 
-  def mul(f: jojo_t, g: jojo_t): jojo_t
+  def mul(f: elem_t, g: elem_t): elem_t
 
-  def cut_respect_mul(f: jojo_t, g: jojo_t) = {
+  def cut_respect_mul(f: elem_t, g: elem_t) = {
     eqv(cut(mul(f, g)), mul(cut(f), cut(g)))
   }
 
-  def mul_associative(f: jojo_t, g: jojo_t, h: jojo_t) = {
+  def mul_associative(f: elem_t, g: elem_t, h: elem_t) = {
     eqv(mul(f, mul(g, h)), mul(mul(f, g), h))
   }
 
-  def id: jojo_t
+  def id: elem_t
 
-  def id_left(f: jojo_t) = {
+  def id_left(f: elem_t) = {
     eqv(f, mul(id, f))
   }
 
-  def id_rigth(f: jojo_t) = {
+  def id_rigth(f: elem_t) = {
     eqv(f, mul(f, id))
   }
 
@@ -29,13 +29,13 @@ abstract class adventure_t {
     assert(cut(id) == id)
   }
 
-  def error: jojo_t
+  def error: elem_t
 
-  def error_left(f: jojo_t) = {
+  def error_left(f: elem_t) = {
     eqv(error, mul(error, f))
   }
 
-  def error_right(f: jojo_t) = {
+  def error_right(f: elem_t) = {
     eqv(error, mul(f, error))
   }
 

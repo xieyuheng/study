25c4bf412af6b6007b1fa9a43b2fe18ffbde5c3a
Xie Yuheng <xyheme@gmail.com>
2019-12-09 20:24:12 +0800

    up

--- src/main/scala/xieyuheng/cicada/Ctx.scala
+++ src/main/scala/xieyuheng/cicada/Ctx.scala
@@ -7,7 +7,7 @@ case class Ctx(map: Map[String, Val] = Map()) {
   }
 
   def ext(name: String, value: Val): Env = {
-    Env(map + (name -> value))
+    Env(this.map + (name -> value))
   }
 
   def ext_map(map: Map[String, Val]): Env = {
--- src/main/scala/xieyuheng/cicada/infer.scala
+++ src/main/scala/xieyuheng/cicada/infer.scala
@@ -11,23 +11,42 @@ object infer {
           case Some(s) => Right(s)
           case None => Left(Err(s"can not find var: ${name} in ctx"))
         }
+
       case Type() =>
         Right(ValType())
+
       case Pi(arg_map: ListMap[String, Exp], ret_type: Exp) =>
         Right(ValType())
+
       case Fn(arg_map: ListMap[String, Exp], body: Exp) =>
-        ???
+        for {
+          ctx <- infer_ctx_from_telescope(ctx, arg_map)
+          // TODO need to construct a Pi type here
+          //   which means 
+          result <- infer(ctx, body)
+        } yield result
+
       case Ap(target: Exp, arg_list: List[Exp]) =>
         ???
+
       case Cl(type_map: ListMap[String, Exp]) =>
         Right(ValType())
+
       case Obj(val_map: ListMap[String, Exp]) =>
         ???
+
       case Dot(target: Exp, field: String) =>
         ???
+
       case Block(let_map: ListMap[String, Exp], body: Exp) =>
         ???
     }
+  }
+
+  def infer_ctx_from_telescope(
+    ctx: Ctx, telescope: ListMap[String, Exp],
+  ): Either[Err, Ctx] = {
+    ???
   }
 
 }
